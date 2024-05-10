0d743739435b0fbddea9bcefa885937f9367ce81
Your Name <youremail@domain.com>
2024-05-10 17:12:28 +0530

    add new code

--- task.py
+++ task.py
@@ -1,6 +1,11 @@
-num = list(range(10))
-previousNum = 0
-for i in num:
-    sum = previousNum + i
-    print('Current Number '+ str(i) + 'Previous Number' + str(previousNum) + 'is ' + str(sum))
-    previousNum=i
+import argparse
+n = argparse.ArgumentParser(description='process some integers')
+n.add_argument('current_number', type=int)
+n.add_argument('previous_number', type=int)
+b = n.parse_args()
+current_num = b.current_number
+previous_num = b.previous_number
+for i in range(10):
+    sum = previous_num + i
+    print(f'Current number {i} Previous Number {previous_num} is {sum}')
+    previous_num = i
