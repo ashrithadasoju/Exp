7c2bfd5b00ffe07f54895a7111b8dde94171ed9c
Your Name <youremail@domain.com>
2024-05-10 16:38:59 +0530

    add new code

--- histogram.py
+++ histogram.py
@@ -1,14 +1,17 @@
 
 import numpy as np
-import cv2 as cv
+import cv2
 from matplotlib import pyplot as plt
- 
-img = cv.imread('/home/ashritha-dasoju/Desktop/my_folder/tom_jerry.jpg')
-cv.imwrite("/home/ashritha-dasoju/Desktop/tom.jpg",img)
-assert img is not None, "file could not be read, check with os.path.exists()"
+import argparse
+h = argparse.ArgumentParser()
+h.add_argument('image', help='enter the image path')
+h.add_argument('output', help='enter the output path')
+args = vars(h.parse_args())
+image = cv2.imread(args['image'])
+assert image is not None, "file could not be read, check with os.path.exists()"
 color = ('b','g','r')
 for i,col in enumerate(color):
- histr = cv.calcHist([img],[i],None,[256],[0,256])
+ histr = cv2.calcHist([image],[i],None,[256],[0,256])
  plt.plot(histr,color = col)
  plt.xlim([0,256])
 plt.show()
